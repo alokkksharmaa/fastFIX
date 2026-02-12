ae4b247561e3c48567412f2c770577b7513211e0
alokkksharmaa <aloksharma1097@gmail.com>
2026-02-12 15:38:22 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import User from './schema/user.js';
-import user from './schema/user.js';
-// import Admin from './schema/admin.js';
+// import Admin from './schema/admin.js'
 
 const app = express();
 const PORT = 3000;
@@ -17,7 +16,7 @@ app.get("/", (req, res) => {
 });
 
 
-// Create User
+// post 
 app.post("/api/users", async (req, res) => {
   try {
     const newUser = await User.create(req.body);
@@ -27,15 +26,39 @@ app.post("/api/users", async (req, res) => {
   }
 });
 
-app.get("/api/users", async (req, res) => {
+// get
+app.get("/api/users/:id", async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.findById();
     res.status(200).json(users)
   } catch (error) {
     res.status(400).json(error);
   }
 })
 
+// put
+app.put("/api/users/:id", async (req, res) => {
+  try {
+    const updateuser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true })
+    res.status(200).json(updateuser)
+  } catch (error) {
+    res.status(404).json(error);
+  }
+})
+
+
+// delete
+app.delete("/api/users/:id", async (req, res) =>{
+  try{
+    const deleteUser  = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json(deleteUser);
+  }catch(error){
+    res.status(400).json({ message: error.message });
+  }
+})
 
 // app.post("/api/admin", async (req, res) => {
 //   try {
