c49cdbbddfafc410ce458a388e6d1c12cc3012c2
alokkksharmaa <aloksharma1097@gmail.com>
2026-02-12 13:34:07 +0530

    db tested

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import User from './schema/user.js';
+import user from './schema/user.js';
+// import Admin from './schema/admin.js';
 
 const app = express();
 const PORT = 3000;
-const DB_URL = 'mongodb://localhost:27017/myapp';
+const DB_URL = "mongodb://localhost:27017/backend";
 
 // Middleware
 app.use(express.json());
@@ -14,9 +16,6 @@ app.get("/", (req, res) => {
   res.send("hello from home");
 });
 
-app.get("/about", (req, res) => {
-  res.send("hello from about");
-});
 
 // Create User
 app.post("/api/users", async (req, res) => {
@@ -27,6 +26,25 @@ app.post("/api/users", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(user)
+  } catch (error){
+    res.status(400).json(error);
+  }
+})
+
+
+// app.post("/api/admin", async (req, res) => {
+//   try {
+//     const newAmdin = await Admin.create(req.body);
+//     res.status(201).json(newAmdin);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message })
+//   }
+// })
 
 // DB Connection + Server Start
 mongoose.connect(DB_URL)
