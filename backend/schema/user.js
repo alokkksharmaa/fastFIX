fe4cd3201b67f9c941041cccab6e6a53910126ad
alokkksharmaa <aloksharma1097@gmail.com>
2026-02-12 12:29:03 +0530

    Add user schema

---
+++ backend/schema/user.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name : {
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    unique: true,
+    
+  }
+})
